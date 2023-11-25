3394f1171a22dca811f29ea6f96ee2e23d038ed8
Sara Cox <sara.zacs@gmail.com>
2023-11-25 11:09:33 -0500

    promitionRouter and partnerRouter

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -21,4 +21,24 @@ campsiteRouter.route('/')
     res.end('Deleting all campsites');
 });
 
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200; 
+    res.setHeader('Content-Type', 'text/plain')
+    next()
+})
+.get((req, res) => {
+    res.end(`Will get you campsite with id: ${req.params.campsiteId}`)
+})
+.post((req, res) => {
+    res.statusCode = 403; 
+    res.end('POST operation not supported on /campsites/:campsiteId')
+})
+.put((req, res) => {
+    res.end(`Updating campsite with new name of ${req.body.name} and description: ${req.body.description}`)
+})
+.delete((req, res) => {
+    res.end(`Deleting campsite with id of ${req.params.campsiteId}`)
+})
+
 module.exports = campsiteRouter;
